4fddfdded38ef78d5fee3a6039531b75df81c28a
maktechdev <69475978+fullTimeCode@users.noreply.github.com>
2023-06-24 05:06:48 +0200

    Add van detail

--- pages/VanDetail.jsx
+++ pages/VanDetail.jsx
@@ -2,8 +2,29 @@ import React from "react"
 import { useParams } from "react-router-dom"
 
 export default function VanDetail() {
+    const [van, setVan] = React.useState({})
     const params = useParams()
-    console.log(params)
+
+    React.useEffect(() => {
+        fetch(`/api/vans/${params.id}`)
+            .then(res => res.json())
+        .then( data => setVan(data.vans))
+    }, [params])
     
-    return <h1>Van detail page goes here</h1>
+    return (
+        <>
+            {
+                van ? (<main className="van-detail">
+                    <header>
+                        <img src={ van.imageUrl } alt={van.name + " image"} />
+                    </header>
+
+                    <h2> {van.name} </h2>
+                    <p> ${van.price}<span>/day</span></p>
+                     <p> {van.description}</p>
+                    
+                </main> ):<h1>Loading...</h1>
+            }
+        </>
+    )
 }
