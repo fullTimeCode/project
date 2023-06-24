2d4cb6b02faf6726c638e5e86c5a06f315344bcc
maktechdev <69475978+fullTimeCode@users.noreply.github.com>
2023-06-24 12:28:48 +0200

    Rename the site

--- index.jsx
+++ index.jsx
@@ -12,7 +12,7 @@ function App() {
   return (
     <BrowserRouter>
       <header>
-        <Link className="site-logo" to="/">#VanLife</Link>
+        <Link className="site-logo" to="/">#ExcitingVanLife</Link>
         <nav>
           <Link to="/about">About</Link>
           <Link to="/vans">Vans</Link>
