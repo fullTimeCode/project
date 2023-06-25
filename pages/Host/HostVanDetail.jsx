9d5b3d444934fe19897a0b4c2db39f286e118c62
maktechdev <69475978+fullTimeCode@users.noreply.github.com>
2023-06-25 15:33:57 +0200

    Change the van-type style

--- pages/Host/HostVanDetail.jsx
+++ pages/Host/HostVanDetail.jsx
@@ -15,15 +15,21 @@ const HostVanDetail = () => {
   
   return (
     <main className='current-host-van-container'>
+      {/* to=“..”  # one level up by default to parent route. Adding
+      relative=“path” changes to path from default of route  and now it will go one level up in path ( from host/vans/1 to host/van ) and not to the parent route 😀 */}
+
       <Link to=".." relative='path' className='back-link' > <span>&#x2190;</span><span>back to all vans</span></Link>
+      
+   
+
       <header className="current-host-van-header">
         <div className="current-host-van-image">
           <img src={currentVan.imageUrl} alt="" />
         </div>
         <div className="current-host-van-info">
-          <p className="van-type"> {currentVan.type} </p>
-          <h3 className="van-name"> {currentVan.name} </h3>
-          <p className="van-price"> ${currentVan.price}<span>/day</span></p>
+          <p className="current-host-van-type"> {currentVan.type} </p>
+          <h3 className="current-host-van-name"> {currentVan.name} </h3>
+          <p className="current-host-van-price"> ${currentVan.price}<span>/day</span></p>
         </div>   
       </header>
       <div className="current-host-van-links">
@@ -45,9 +51,7 @@ const HostVanDetail = () => {
           <strong>Visibility</strong>: Public
         </p>
       </div>
-
-    
-    </main>
+  </main>
   )
 }
 
